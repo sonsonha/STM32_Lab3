a4275c3846ed4088a1b53723188149a7f9c7d738
sonsonha <ha.danh2002@hcmut.edu.vn>
2023-11-02 10:39:31 +0700

    Ex1

---
+++ Src_LAB_3/Core/Inc/timer.h
@@ -0,0 +1,14 @@
+/*
+ * timer.h
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: sonha
+ */
+
+#ifndef INC_TIMER_H_
+#define INC_TIMER_H_
+
+ void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim );
+
+
+#endif /* INC_TIMER_H_ */
---
+++ Src_LAB_3/Core/Src/timer.c
@@ -0,0 +1,18 @@
+/*
+ * timer.c
+ *
+ *  Created on: Nov 2, 2023
+ *      Author: sonha
+ */
+
+#include "main.h"
+#include "input_reading.h"
+
+ void HAL_TIM_PeriodElapsedCallback ( TIM_HandleTypeDef * htim )
+ {
+ if(htim -> Instance == TIM2 ) {
+ button_reading () ;
+ }
+}
+
+
